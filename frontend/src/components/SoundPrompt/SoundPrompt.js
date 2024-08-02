a67796c0f69ba20c005259dd283b0192f63723f8
awr7 <147633136+awr7@users.noreply.github.com>
2024-08-01 20:26:44 -0400

    added glitchy text animation. thank you hyperplexed!

--- frontend/src/components/SoundPrompt/SoundPrompt.js
+++ frontend/src/components/SoundPrompt/SoundPrompt.js
@@ -1,26 +1,65 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './SoundPrompt.css';
 
 const SoundPrompt = ({ onAccept, onDeny }) => {
   const [hovered, setHovered] = useState(null);
+  const letters = 'abcdefghijklmnopqrstuvwxyz';
+  const [acceptText, setAcceptText] = useState('Accept');
+  const [denyText, setDenyText] = useState('Deny');
+
+  useEffect(() => {
+    const animateText = (text, setText) => {
+      let interval = null;
+      let iteration = 0;
+
+      interval = setInterval(() => {
+        setText(prev => 
+          prev.split("")
+            .map((letter, index) => {
+              if (index < iteration) {
+                return text[index];
+              }
+              return letters[Math.floor(Math.random() * letters.length)];
+            })
+            .join("")
+        );
+
+        if (iteration >= text.length) {
+          clearInterval(interval);
+        }
+
+        iteration += 1 / 5;
+      }, 30);
+
+      return () => clearInterval(interval);
+    };
+
+    const clearAcceptAnimation = animateText('Accept', setAcceptText);
+    const clearDenyAnimation = animateText('Deny', setDenyText);
+
+    return () => {
+      clearAcceptAnimation();
+      clearDenyAnimation();
+    };
+  }, [letters]);
 
   return (
     <div className="sound-prompt">
       <div className="title">Master Chess with AI</div>
       <div className="gradient-line"></div>
-      <div class="prompt-message-container">
-        <div class="prompt-message">
-          <span class="typing-text">This website is best experienced with music. Would you like the sound on?</span>
+      <div className="prompt-message-container">
+        <div className="prompt-message">
+          <span className="typing-text">This website is best experienced with music. Would you like the sound on?</span>
         </div>
       </div>
-      <div class="prompt-buttons">
+      <div className="prompt-buttons">
         <div
           className={`menu-item ${hovered === 'accept' ? 'menu-item-hovered' : ''}`}
           onMouseEnter={() => setHovered('accept')}
           onMouseLeave={() => setHovered(null)}
           onClick={onAccept}
         >
-          Accept
+          {acceptText}
         </div>
         <div
           className={`menu-item ${hovered === 'deny' ? 'menu-item-hovered' : ''}`}
@@ -28,12 +67,12 @@ const SoundPrompt = ({ onAccept, onDeny }) => {
           onMouseLeave={() => setHovered(null)}
           onClick={onDeny}
         >
-          Deny
+          {denyText}
         </div>
       </div>
       <div className="gradient-line-jr"></div>
     </div>
   );
-};
+}
 
 export default SoundPrompt;
